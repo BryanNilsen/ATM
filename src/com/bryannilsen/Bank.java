8fb5719c8909b1553dd84c6e1f87164279a6248f
BryanNilsen <bryannilsen@gmail.com>
2020-08-28 13:07:46 -0500

    classes updated with login and password check - 49:27

--- src/com/bryannilsen/Bank.java
+++ src/com/bryannilsen/Bank.java
@@ -73,11 +73,50 @@ public class Bank {
         return uuid;
     }
 
-    /**
-     * Add an account for the bank
-     * @param anAcct    the account to add
-     */
-    public void addAccount(Account anAcct) {
+    public void addAccount(Account anAcct){
         this.accounts.add(anAcct);
+    }
+
+    /**
+     * Create a new user of the bank
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param pin       the user's pin
+     * @return          the new User object
+     */
+
+    public User addUser(String firstName, String lastName, String pin) {
+
+        // create a new User object and add it to our list
+        User newUser = new User(firstName, lastName, pin, this);
+        this.users.add(newUser);
+
+        // create a savings account for the user and add to User and Bank
+        // accounts lists
+        Account newAccount = new Account("Savings", newUser, this);
+        newUser.addAccount(newAccount);
+        this.addAccount(newAccount);
+
+        return newUser;
+    }
+
+    /**
+     * Get the User object associated with a particular userID and pin
+     * if they are valid
+     * @param userID    the UUID of the user to log in
+     * @param pin       the pin of the user
+     * @return          the User object if the login is successful, or null
+     *                  if it is not
+     */
+    public User userLogin(String userID, String pin){
+        //  search through list of users
+        for (User u : this.users){
+            // check user ID is correct
+            if (u.getUUID().compareTo(userId) == 0 && u.validatePin(pin)) {
+                return u;
+            }
+        }
+        // if we haven't found the user of have an incorrect pin
+        return null;
     }
 }
